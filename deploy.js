c6895c1f1309cacdc719d2beb12a9dee666de06e
um4ng-squbix <umang.tiwari@squbix.com>
2021-12-29 19:50:32 +0530

    first commit

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const dotenv = require("dotenv").config();
+const { interface, bytecode } = require("./compile");
+
+// Takes mnemonic and url to network
+const provider = new HDWalletProvider(
+  process.env.MNEMONIC_PHRASE,
+  process.env.CHAIN_URL
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
